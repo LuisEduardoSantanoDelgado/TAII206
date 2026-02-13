39a8bd7c4c1ce851a71c540c5115c3099511da33
LuisEduardoSantanoDelgado <123046272@upq.edu.mx>
2026-02-12 21:04:23 -0600

    put delete

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,44 +1,71 @@
-#importaciones
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import Optional
 import asyncio
 
-#inicializacion
-app= FastAPI(
-    title= 'Mi Primer API',
+# Inicialización
+app = FastAPI(
+    title='Mi Primer API',
     description='Luis Eduardo Santano Delgado',
     version='1.0',
 )
 
-
-usuarios=[
-    {"id":1,"Nombre":"Anuel","edad":32},
-    {"id":2,"Nombre":"Mauricio","edad":20},
-    {"id":3,"Nombre":"Luis Eduardo","edad":21},
+usuarios = [
+    {"id": 1, "Nombre": "Anuel", "edad": 32},
+    {"id": 2, "Nombre": "Mauricio", "edad": 20},
+    {"id": 3, "Nombre": "Luis Eduardo", "edad": 21},
 ]
 
-#EndPoint
-
+# EndPoint
 @app.get("/", tags=['inicio'])
 async def HolaAnuel():
-    return{"mensaje":"Hola Mundo FastAPI"}
-
+    return {"mensaje": "Hola Mundo FastAPI"}
 
 @app.get("/v1/bienvenidos", tags=['inicio'])
 async def Bienvenidos():
-    return{"mensaje":"Bienvenidos a tu FastAPI"}
+    return {"mensaje": "Bienvenidos a tu FastAPI"}
 
 @app.get("/v1/calificaciones", tags=['Asincronia'])
 async def calificaciones():
     await asyncio.sleep(5)
-    return{"mensaje":"Tu calificacion en TAI es 10"}
+    return {"mensaje": "Tu calificacion en TAI es 10"}
 
 @app.get("/v1/usuarios/{id}", tags=['Parametro obligatorio'])
-async def consultaUsuarios(id:int):
-    return{"Usuario Encontrado":id}
+async def consultaUsuarios(id: int):
+    return {"Usuario Encontrado": id}
 
 @app.get("/v1/usuarios_op/", tags=['Parametro opcional'])
-async def consultaOp(id: Optional[int]=None):
+async def consultaOp(id: Optional[int] = None):
     await asyncio.sleep(2)
     if id is not None:
-     return{"Usuario Encontrado": id }
+        return {"Usuario Encontrado": id}
+
+@app.put("/v1/usuarios/{id}", tags=['CRUD Usuarios'])
+async def actualizar_usuario(id: int, usuario: dict):
+    for idx, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            usuarios[idx] = {**usr, **usuario}
+            return {
+                "mensaje": "Usuario actualizado",
+                "datos": usuarios[idx],
+                "status": "200"
+            }
+
+    raise HTTPException(
+        status_code=400,
+        detail="Usuario no encontrado"
+    )
+
+@app.delete("/v1/usuarios/{id}", tags=['CRUD Usuarios'])
+async def eliminar_usuario(id: int):
+    for idx, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            del usuarios[idx]
+            return {
+                "mensaje": "Usuario eliminado",
+                "status": "200"
+            }
+    
+    raise HTTPException(
+        status_code=400,
+        detail="Usuario no encontrado"
+    )
